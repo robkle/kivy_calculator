75a4beee6db16e3b5e827e89713e2337653f2d79
robkle <springbokvlakte@yahoo.com>
2021-06-04 17:02:42 +0300

    Initial commit

---
+++ checker.py
@@ -0,0 +1,4 @@
+class InputChecker():
+	def checker(input_char, calc_input):
+		return True
+	#Add functions that check valid use of operands and operators
---
+++ main.py
@@ -0,0 +1,34 @@
+import kivy
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.lang import Builder
+from kivy.core.window import Window
+from checker import *
+
+
+Window.size = (500 , 700)
+
+Builder.load_file('calc.kv')
+
+class CalcLayout(GridLayout):
+	def backspace(self, calc_input):
+		if calc_input == "Error":
+			self.display.text = ""
+		else:
+			self.display.text = calc_input[:-1]
+	def checker(self, input_char, calc_input):
+		if InputChecker.checker(input_char, calc_input):
+			self.display.text = calc_input + input_char
+	def calculate(self, calc_input):
+		try:
+			self.display.text = str(eval(calc_input))
+		except Exception:
+			self.display.text = "Error"
+
+
+class CalculatorApp(App):
+	def build(self):
+		return CalcLayout()
+
+if __name__ == "__main__":
+	CalculatorApp().run()
